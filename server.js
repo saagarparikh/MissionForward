f2fc7bfc87e68cbcd95e0b149f675d28a4efee90
saagarparikh <saagarparikh24@gmail.com>
2020-04-09 23:30:15 -0700

    heroku deployment fixes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const alumniDataRouter = require('./routes/alumdata');
 app.use('/alumni-data', alumniDataRouter);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(esxpress.static('client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
